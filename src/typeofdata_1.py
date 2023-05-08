6edc73e6a0a45784c894640648c624b46ca91aa7
benjamin <benjaminatai@gmail.com>
2023-05-08 14:12:38 -0300

    firstStep

---
+++ src/typeofdata_1.py
@@ -0,0 +1,33 @@
+a = 5
+b = "benjamin"
+c = 5.13
+d = True
+
+print(f"Int:{type(a)}")
+print(f"String:{type(b)}")
+print(f"Float:{type(c)}")
+print(f"Boolean:{type(d)}")
+
+# This is a comment
+
+"""
+This is a other comment
+"""
+age = 21
+print(f"Hi benja,your age is:{age}")
+
+print("hello" * 4)
+
+var = "Hello World!"
+var = var + ", I'm twenty one years"
+print(var)
+print(var[1])
+print(var[:5])  # Hello
+print(len(var))  # length
+print(var.upper())  # mayuscula
+print(var.lower())  # minisculas
+print(var.capitalize())  # firs letter in mayuscula
+print(var.replace("e","a")) #replace the letters
+
+print("   Juan perez vive en barrio jardin".strip())  # elimina espacios al principio
+print("hello benjamin\nsalto de linea:\tespacio")
