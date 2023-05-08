4df533d5925fd4a85bac1286076734b39582cb8d
benjamin <benjaminatai@gmail.com>
2023-05-08 14:29:07 -0300

    Operators_2

---
+++ src/aritmetic_2.py
@@ -0,0 +1,16 @@
+# Operators
+print(52 + 10)
+print(10 - 7)
+
+print(2 * 5)
+print(8 / 2)
+print(5 ** 2)
+print(10 // 2)
+print(12 % 2)
+
+print(41 < 10)
+print(10 > 5)
+print(5 != 2)
+print(42 == 42)
+print(5 > 1 and 5 < 10)
+print(5 < 7 or 5 > 7)
