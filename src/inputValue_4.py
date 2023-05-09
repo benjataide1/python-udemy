d71dc596442c51924fb8857dcfad6a076274bace
benjamin <benjaminatai@gmail.com>
2023-05-09 12:48:49 -0300

    InputValue

---
+++ src/inputValue_4.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+print(f"Your name is:{name}\n")
+# print("YOur names is: " + name )
+print(f"hello{name}" * 4, "\n")
+
+age = int(input("Please ,Enter your Age: "))  # I need change my input for one int ,because ,input return one number in String
+print(f"your name is: {name} and your age is: {age}\n")
+
+total = age + 51
+print(f"The total is: {total}\n")
