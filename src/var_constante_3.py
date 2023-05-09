5783b9e8a775513bebb392836ea349235d574248
benjamin <benjaminatai@gmail.com>
2023-05-09 12:48:41 -0300

    variablesConstantes_3

---
+++ src/var_constante_3.py
@@ -0,0 +1,30 @@
+# we give value to variables
+
+name = "benjamin"
+age = 10
+number = 11.00
+edad = 21
+# show the result
+print("my age is: " + str(age))  # convert int to string
+
+# calculate the total
+total = age + number
+print(f"The total is:{total}")
+
+# delete variable
+# del edad
+# print(edad)
+#
+
+# we assing other values
+age = 89
+number = 20
+print(age, number)
+
+# !Constante -> in Mayuscula , the value can be change
+ANHOS = 21
+print(ANHOS)
+
+# I can ,assing various values
+a, b, c, d = 1.4, 7, "ataide", True
+print(f"a:{a}\nb:{b}\nc:{c}\nd:{d}")
