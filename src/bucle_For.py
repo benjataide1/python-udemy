328d6379309e986aff995bfdf0de1fe53ba799cd
benjamin <benjaminatai@gmail.com>
2023-05-11 11:52:56 -0300

    For

--- src/bucle_For.py
+++ src/bucle_For.py
@@ -7,10 +7,14 @@ for count in range(since, until):
     total = number * count
     print(f"{number} * {count}: {total}")
 
+print('----------------------------------')
+
 # !List
 nombre = ["benjamin", "jazmin", "fran", "lorezno"]
 for i in nombre:
     print(f"Nombre:{i}")
+
+print('----------------------------------')
 
 # !Reverse Table
 reverseNumber = int(input("Please Enter the Number,Reverse Table: "))
@@ -21,3 +25,32 @@ until = 0
 for i in range(since, until, -1):
     total = reverseNumber * i
     print(f"{reverseNumber} * {i}: {total}")
+
+print('----------------------------------')
+
+# EXTRA
+
+list_colors = ["blue", "red", "green", "blue marine", "pink"]
+
+for color in list_colors:
+    if color == 'gray':
+        print("Not found the color")
+    elif color == 'blue':
+        print("color found")
+        break
+    else:
+        print("error")
+        break
+
+print('----------------------------------')
+
+range_lenght = range(1, 101)
+print(range_lenght)
+
+for number in range_lenght:
+    if (number == 21):
+        print("your age")
+        break
+    elif (number == 25):
+        print("found it")
+        break
