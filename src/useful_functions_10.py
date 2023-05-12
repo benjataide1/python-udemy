16ccbc9b9e126d4e602e080416bc4e877481d1f7
benjamin <benjaminatai@gmail.com>
2023-05-12 12:58:36 -0300

    utilidades en listas

---
+++ src/useful_functions_10.py
@@ -0,0 +1,20 @@
+#redondeo
+print(round(3,4563))
+
+#valor absoluto
+print(abs(-57))
+
+
+#crear una lista
+lista = [1,5,2,7,4,6,41,24]
+#minimo de la lista
+print(min(lista))
+#maximo de la lista
+print(max(lista))
+
+
+#orderna la lista
+print(sorted(lista))
+
+#suma total de la lista
+print(sum(lista))
