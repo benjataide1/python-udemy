5d9ca202cccde24860a240527a7148e1a2d5759d
benjamin <benjaminatai@gmail.com>
2023-05-15 14:21:36 -0300

    package_function_sum and isPar

---
+++ src/package/function.py
@@ -0,0 +1,12 @@
+# functions
+def isPar(number):
+    if (number % 2 == 0):
+        # print("the number is Par")
+        return True
+    else:
+        # print("the number is Impar")
+        return False
+
+
+def sum(num1, num2):
+    return num1 + num2
