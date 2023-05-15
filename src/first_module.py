c9806e4b49e3736439b209176af46b4b041a3614
benjamin <benjaminatai@gmail.com>
2023-05-15 13:01:26 -0300

    module_01

---
+++ src/first_module.py
@@ -0,0 +1,9 @@
+# import modules
+import math
+
+# from math import sqrt --> only this function
+
+numer = int(input("enter one number and I show you,the root:"))
+print("The square root the: " + str(numer) + ", is: " + str(math.sqrt(numer)))
+
+
