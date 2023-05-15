8bf781c26bac437076cab3e50c848895edbc06fd
benjamin <benjaminatai@gmail.com>
2023-05-15 14:21:51 -0300

    teste_importPackage

---
+++ src/package_import.py
@@ -0,0 +1,14 @@
+# import package.function
+from package.function import *
+
+# from package.function import sum
+# from package.function import isPar
+
+
+print(isPar(25))  # here I call the funcion isPar fo the archive package
+print(isPar(30))
+
+# sum
+print(sum(412, 31))
+print(sum(64, 77))
+
