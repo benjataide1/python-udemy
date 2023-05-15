09efcfad43d03e1d9b5eb71b6a11ad6cbb94e67f
benjamin <benjaminatai@gmail.com>
2023-05-15 14:22:12 -0300

    createModule

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="myFunction",
+    version="1.0",
+    description="realizado suma y verificar par o impar",
+    author="benjamin",
+    author_email="benjaminatai@gmail.com",
+    url="https://www.ticolticotta.es",
+    packages=["package"]
+)
